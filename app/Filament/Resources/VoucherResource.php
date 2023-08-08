58a826ff9756ec6158c122f1fb1108f48a8d2ba9
abdullah6516 <61629432+abdullah6516@users.noreply.github.com>
2023-08-08 11:56:51 +0300

    [add] second model voucher

---
+++ app/Filament/Resources/VoucherResource.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\VoucherResource\Pages;
+use App\Filament\Resources\VoucherResource\RelationManagers;
+use App\Models\Voucher;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class VoucherResource extends Resource
+{
+    protected static ?string $model = Voucher::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //Forms\Components\BelongsToSelect::make('product_id')
+                //->relationship('product', 'name')
+                Forms\Components\TextInput::make('code')
+                    ->required()
+                    ->unique(),
+                Forms\Components\TextInput::make('discount_percent')
+                    ->label('Discount (%)')
+                    ->numeric()
+                    ->default(10)
+                    ->extraInputAttributes(['min' => 1, 'max' => 100, 'step' => 1]),
+
+                Forms\Components\Select::make('product_id')
+                    ->relationship('product', 'name')
+            ]);
+
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('code'),
+                Tables\Columns\TextColumn::make('discount_percent')->label('discount (%)'),
+                Tables\Columns\TextColumn::make('product.name'),
+                Tables\Columns\TextColumn::make('payments_count')
+                    ->counts('payments')
+                    ->label('Times used'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVouchers::route('/'),
+            'create' => Pages\CreateVoucher::route('/create'),
+            'edit' => Pages\EditVoucher::route('/{record}/edit'),
+        ];
+    }
+}
